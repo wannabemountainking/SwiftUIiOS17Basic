8532ced6d4faf8ec1117059a8a2fb6d911f5b919
wannabemountainking <87057243+wannabemountainking@users.noreply.github.com>
2025-12-24 15:46:59 +0900

    SectorMark
    원형 또는 도넛 차트 SectorMark 만들기

---
+++ SwiftUIiOS17Basic/ContentUnavailableViewiOS17.swift
@@ -0,0 +1,18 @@
+//
+//  ContentUnavailableViewiOS17.swift
+//  SwiftUIiOS17Basic
+//
+//  Created by YoonieMac on 12/24/25.
+//
+
+import SwiftUI
+
+struct ContentUnavailableViewiOS17: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentUnavailableViewiOS17()
+}
