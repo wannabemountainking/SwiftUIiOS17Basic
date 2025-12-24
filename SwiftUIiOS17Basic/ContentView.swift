b6e0b191295e0ab6aaa709be167a7c2e28da55d4
wannabemountainking <kdy0096@gmail.com>
2025-12-24 15:13:04 +0900

    Initial Commit

---
+++ SwiftUIiOS17Basic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIiOS17Basic
+//
+//  Created by YoonieMac on 12/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIiOS17Basic/SwiftUIiOS17BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiOS17BasicApp.swift
+//  SwiftUIiOS17Basic
+//
+//  Created by YoonieMac on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiOS17BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
