be4bc4b1de12b9da12701aabb9f88dfd9d6268e6
wannabemountainking <87057243+wannabemountainking@users.noreply.github.com>
2025-12-25 15:42:06 +0900

    ContentUnavailableView SymbolEffect

--- SwiftUIiOS17Basic/ContentUnavailableViewiOS17.swift
+++ SwiftUIiOS17Basic/ContentUnavailableViewiOS17.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct ContentUnavailableViewiOS17: View {
+    
+// MARK: - ContentUnavailable : Image와 Text를 합쳐서 전체 스크린에 나타냄 -> 말 그대로 Content가 unavailable 즉 사용할 수 없는 상태 UI에 Error 표시할 때 유용함.
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ContentUnavailableView("BAD CONNECTION", systemImage: "wifi.exclamationmark", description: Text("인터넷이 연결되어 있지 않습니다."))
+        
+        Divider()
+        
+        ContentUnavailableView {
+            // label
+            Label("정말로 삭제하시겠습니까?", systemImage: "questionmark.circle.fill")
+        } description: {
+            Text("아래 삭제 버튼을 누르면 영구 삭제 됩니다.")
+        } actions: {
+            HStack(spacing: 30) {
+                Button("Cancel", role: .cancel) {
+                    //Cancel Action
+                }
+                Button("Delete", role: .destructive) {
+                    // Delete Action
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .fixedSize()
+        }
     }
 }
 
-#Preview {
+struct ContentUnavailablePaging: View {
+    var body: some View {
+        // TabView를 이용하여 간단하게 page 형식으로도 만들 수 있습니다.
+        TabView {
+            ContentUnavailableView("SwiftUI", image: "mobile1", description: Text("SwiftUI로 여러 가지 App을 만들 수 있습니다"))
+            ContentUnavailableView("Sync", image: "mobile2", description: Text("아이폰과 아이패드를 iCloud로 동기화 합니다"))
+            ContentUnavailableView("Shopping App", image: "mobile3", description: Text("온라인 쇼핑 App을 SwiftUI로 만듭니다"))
+        }
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+    }
+}
+
+#Preview("Intro") {
     ContentUnavailableViewiOS17()
+}
+
+#Preview("Paging") {
+    ContentUnavailablePaging()
 }
---
+++ SwiftUIiOS17Basic/SymbolEffectiOS17.swift
@@ -0,0 +1,54 @@
+//
+//  SymbolEffectiOS17.swift
+//  SwiftUIiOS17Basic
+//
+//  Created by YoonieMac on 12/25/25.
+//
+
+import SwiftUI
+
+// SymbolEffect 를 통해서 SF Symbol에 animation 효과를 넣을 수 있습니다.
+struct SymbolEffectiOS17: View {
+    @State private var startAnimation: Bool = false
+    
+    var body: some View {
+        List {
+            Label {
+                //title
+                Text("색 변형 - variableColor")
+            } icon: {
+                Image(systemName: "airplayaudio")
+                    .foregroundStyle(.mint)
+                    .symbolEffect(.variableColor)
+            }
+            
+            Label {
+                //title
+                Text("경고 표시 알림 - Pulse")
+            } icon: {
+                Image(systemName: "airplayaudio.badge.exclamationmark")
+                    .foregroundStyle(.red, .mint)
+                    .symbolEffect(.pulse)
+            }
+            
+            Label {
+                //title
+                Text("Start Button 누르면 시작")
+            } icon: {
+                // Value 값이 변하면 Animation 시작
+                Image(systemName: "lightbulb.max.fill")
+                    .foregroundStyle(.mint)
+                    .symbolEffect(.bounce, options: .repeating.repeat(2), value: startAnimation)
+            }
+            
+            Button("Start Button") {
+                startAnimation.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+        }
+    }
+}
+
+#Preview {
+    SymbolEffectiOS17()
+}
